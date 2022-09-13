a180aa8c4030a5b6768f0bbb228ff7a92fa88b2f
Jack Spencer <UniformKitten281@gmail.com>
2022-09-13 08:38:28 -0700

    Create partners.js

---
+++ Commands/credits/partners.js
@@ -0,0 +1,11 @@
+// Code By Colack //
+
+module.exports={
+    name:'partners',
+		aliases: ['founders', 'ogs'],
+    description: "Shows all of the partners of the bot.",
+    execute(message, args){
+        message.channel.send('https://discord.gg/BJvmm4bEGm');
+				message.channel.send('https://discord.gg/CccjBhQevZ');
+        }
+      };
