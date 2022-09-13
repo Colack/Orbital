53dae9a41bdaf0384fa228256f59a5eaf59c41e5
Jack Spencer <UniformKitten281@gmail.com>
2022-09-13 08:43:21 -0700

    Create entranslate.js

---
+++ Commands/translate/entranslate.js
@@ -0,0 +1,16 @@
+// Code By Colack //
+
+const Discord = require('discord.js');
+const translate = require("@iamtraction/google-translate");
+
+module.exports = {
+	name: 'entranslate',
+	description: 'translate something.',
+	async execute(message, args){
+		const query = args.join(" ");
+		if (!query) return message.reply('Please type something to translate.');
+
+		const translated = await translate(query, { to: 'en' });
+		message.channel.send(translated.text);
+	}
+}
